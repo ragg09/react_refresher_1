1e213032f1a7ebcb3bbb88b431267226f36829a4
ragg09 <ragunayon@gmail.com>
2022-10-14 09:46:50 +0800

    Part 1 refresher done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,128 +1,132 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef } from 'react'
 import '../reset.css';
 import '../App.css';
+import NoTodos from './NoTodos';
+import TodoForm from './TodoForm';
+import TodoList from './TodoList';
+import useLocalStorage from '../hooks/useLocalStorage';
+import { TodosContext } from '../context/TodosContext';
+import { CSSTransition, SwitchTransition } from 'react-transition-group';
 
 export default function App() {
-    const [todos, setTodos] = useState([
-       {
-            id: 1,
-            title: 'Finised',
-            isComplete: true,
-       },
-       {
-            id: 2,
-            title: 'In progress',
-            isComplete: false,
-       },
-       {
-            id: 3,
-            title: 'Am trying',
-            isComplete: false,
-       } 
-    ]);
+    const [todos, setTodos] = useLocalStorage('todos', []);
+    const [name, setName] = useLocalStorage('name', '')
+    const [idCount, setIdCount] = useLocalStorage('idCount', 0)
+    const nameInputEl = useRef(null);
+    const nodeRefTodoList = React.useRef(null);
+    const nodeRefNoTodoList = React.useRef(null);
 
-
-    const [todoInput, setTodoInput] = useState("");
     
+    useEffect(()=> {
+        nameInputEl.current.focus();
 
-    //event handler || getting value from input
-    function handleInput(event) {
-        setTodoInput(event.target.value);
+        //get data from local storage even after reload
+        // setName(JSON.parse(localStorage.getItem('name')) ?? '');
+
+
+        // // didUnmount
+        // return function testing(){
+        //     console.log("cleaning up");
+        // }
+    },[name])
+
+    // useEffect(()=> {
+    //     console.log("woring");
+    // },[name])
+
+
+    function handleNameInput(event) {
+        setName(event.target.value)
+        // localStorage.setItem('name', JSON.stringify(event.target.value))
     }
-
-    //event handler || deleting todolist using its id
-    function handleDelete(id) {
-        //filtering method uses loop to delete an array if conditions are met
-        setTodos([...todos].filter(todo => todo.id !== id));
-    }
-
-
-    //adding todo list
-    function addTodoList(event) {
-
-        event.preventDefault();
-        // alert('working!!!!');
-
-        //simple trapping
-       if(todoInput.trim().length === 0){
-            setTodoInput("");
-            return;
-       }
-
-        //other than push, you can add value via this syntax
-        setTodos([...todos, {
-            id: todos.length + 1, //common sense
-            title: todoInput, //value was being handled on handleInput
-            isComplete: false,
-        },]);
-
-        //after pushing, state should be cleared for the next usage
-        setTodoInput("");
-
-    }
-
-    // ()=> , this is a callbackfunction, preventing function from running right away on the loop
-
 
   return (
-    <div className='todo-app'>
+    <TodosContext.Provider 
+    value={
+        {   todos, 
+            setTodos, 
+            idCount, 
+            setIdCount, 
+        }
+    }
+    >
+        <div className='todo-app'>
 
-        <h1>{todos.length}</h1>
+            <h1>Total Todo {todos.length}</h1>
 
-        <form action="#" onSubmit={addTodoList}>
-            <input 
-            className="todo-input" 
-            type="text" 
-            placeholder='Enter Text' 
-            value={todoInput} 
-            onChange={handleInput}
-            required
-            />
-        </form>
+            <form className='mb-1' action="#" >
+                    <input 
+                    className="todo-input" 
+                    type="text" 
+                    placeholder='Enter your name' 
+                    ref={nameInputEl}
+                    value={name}
+                    onChange={handleNameInput}
+                    />
+            </form>
 
-        <ul className='todo-list'>
-
-        {/* looping format using map */}
-        {todos.map((todo, index) => (
-            <li className="todo-item-container" key={todo.id}>
-                <div className="todo-item">
-                    {todo.isComplete === true 
-                    ? 
-                    <input type="checkbox" defaultChecked/> 
-                    : 
-                    <input type="checkbox" />
-                    }
-
-                    <span className="todo-item-label">{todo.title}</span>
-                    {/* <input type="text" className="todo-item-input" value="Finish React Series" /> */}
-                </div>
-                <button 
-                className="x-button"
-                onClick={()=>handleDelete(todo.id)}
+            <CSSTransition
+                nodeRef={nodeRefNoTodoList}
+                in={name.length > 0}
+                timeout={300}
+                classNames="slide-vertical"
+                unmountOnExit
                 >
-                        <svg
-                        className="x-button-icon"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth={2}
-                                d="M6 18L18 6M6 6l12 12"
-                            />
-                        </svg>
-                </button>
-            </li>
-        ))}
+
+                <h3 className='mb-3' ref={nodeRefNoTodoList}>Hello, {name}</h3>
+            </CSSTransition>
+
+            
+
+            <TodoForm />
+
+            <SwitchTransition mode='out-in'>
+                <CSSTransition nodeRef={nodeRefTodoList} key={todos.length > 0} timeout={300} classNames="slide-vertical" unmountOnExit>
+                    <div ref={nodeRefTodoList}>
+                        {todos.length > 0 ? (
+                            <TodoList />
+                        ) : (
+                            <NoTodos/>
+                        )}
+                    </div>
+                </CSSTransition>
+            </SwitchTransition>
+
+            {/* {todos.length > 0 ? (
+                <TodoList />
+            ) : (
+                <NoTodos/>
+            )} */}
+
+            {/* <CSSTransition
+                nodeRef={nodeRefTodoList}
+                in={todos.length > 0}
+                timeout={300}
+                classNames="slide-vertical"
+                unmountOnExit
+                >
+                    <div ref={nodeRefTodoList}>
+                        <TodoList />
+                    </div>
+            </CSSTransition>
+
+            <CSSTransition
+                nodeRef={nodeRefNoTodoList}
+                in={todos.length === 0}
+                timeout={300}
+                classNames="slide-vertical"
+                unmountOnExit
+                >
+                    <div ref={nodeRefNoTodoList}>
+                        <NoTodos/>
+                    </div>
+            </CSSTransition> */}
 
 
-        </ul>
 
+        </div>
+    </TodosContext.Provider>
 
-
-
-    </div>
+    
   )
 }
